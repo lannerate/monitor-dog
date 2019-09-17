7d46f81bc664759d846e45e6af327457314cd5f6
hui <zhanghuizaizhe@hotmail.com>
2019-09-18 00:00:27 +0800

    remove target

--- monitor-server/target/classes/script/heaglelog-clean-cron.sh
+++
@@ -1,1 +0,0 @@
-10 0 * * * sh /home/mongodb/local/mongodb/bin/monitorlog-clean.sh
--- monitor-server/target/classes/script/heaglelog-clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
- /home/mongodb/local/mongodb/bin/mongo 199.78.1.32:27017/monitor_log_db --quiet /home/mongodb/local/mongodb/bin/monitorlog.js
